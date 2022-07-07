bc282f96da64bb16fd90cc906e8d525966b460d8
Maredi Rantho <MRantho@oldmutual.com>
2022-07-07 15:57:01 +0200

    change py file name

--- python/newsapp.py
+++ python/newsapp.py
@@ -4,6 +4,6 @@ import pandas as pd
 
 newsapi = NewsApiClient(api_key='57d708bd6e3a46af9761f57ddf85b9d4')
 
-df = newsapi.get_everything(q='Coco Chanel', language='en', from_param='2022-06-24', to='2022-07-01',country='eu')
+df = newsapi.get_everything(q='Coco Chanel', language='en', from_param='2022-06-24', to='2022-07-01')
 
 type(df)
