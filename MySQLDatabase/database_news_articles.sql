6f63fbaab2985f5cc8ac0889ae1cb7fb7aa3caa1
Maredi <maredi.rantho@gmail.com>
2022-07-10 03:51:36 +0200

    modified sql file and added dockerfile for mysql database

--- MySQLDatabase/database_news_articles.sql
+++ MySQLDatabase/database_news_articles.sql
@@ -3,7 +3,7 @@ CREATE DATABASE news;
 use news;
 
 CREATE TABLE sources(
-    source_id varchar(200) not null AUTO_INCREMENT,
+    source_id varchar(200) not null ,
     source_name varchar(200),
     decsription varchar(200),
     source_url varchar(200),
@@ -11,4 +11,28 @@ CREATE TABLE sources(
     source_language varchar(5),
     counrty varchar(2),
     PRIMARY KEY (source_id)
-)
+);
+
+CREATE TABLE toparticles(
+    source varchar(200) not null,
+    author varchar(200),
+    title varchar(200),
+    source_description varchar(200),
+    source_url varchar(200),
+    publishedAt varchar (200),
+    content varchar(200),
+    PRIMARY KEY (source)
+);
+
+
+CREATE TABLE articles(
+    source varchar(200) not null,
+    author varchar(200),
+    title varchar(200),
+    source_description varchar(200),
+    source_url varchar(200),
+    url_to_image varchar(200),
+    publishedAt varchar (200),
+    content varchar(200),
+    PRIMARY KEY (source)
+);
