6ed52f39c73f4bda1f3cf525e801a2282b583a7e
Maredi <maredi.rantho@gmail.com>
2022-07-11 18:13:08 +0200

    update table structures and first run from external python

--- MySQLDatabase/database_news_articles.sql
+++ MySQLDatabase/database_news_articles.sql
@@ -5,34 +5,31 @@ use news;
 CREATE TABLE sources(
     source_id varchar(200) not null ,
     source_name varchar(200),
-    decsription varchar(200),
-    source_url varchar(200),
+    source_description varchar(1000),
+    source_url varchar(500),
     category varchar (200),
     source_language varchar(5),
-    counrty varchar(2),
+    country varchar(2),
     PRIMARY KEY (source_id)
 );
 
 CREATE TABLE toparticles(
-    source varchar(200) not null,
     author varchar(200),
     title varchar(200),
-    source_description varchar(200),
-    source_url varchar(200),
+    source_description varchar(1000),
+    source_url varchar(500),
+    url_to_image varchar(500),
     publishedAt varchar (200),
-    content varchar(200),
-    PRIMARY KEY (source)
+    content varchar(1000)
 );
 
 
 CREATE TABLE articles(
-    source varchar(200) not null,
     author varchar(200),
     title varchar(200),
-    source_description varchar(200),
-    source_url varchar(200),
-    url_to_image varchar(200),
+    source_description varchar(1000),
+    source_url varchar(500),
+    url_to_image varchar(500),
     publishedAt varchar (200),
-    content varchar(200),
-    PRIMARY KEY (source)
+    content varchar(1000)
 );
